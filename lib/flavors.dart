3919dece187a7f438abdbf89ee7f206951efeaf2
adhamkhaled <adhamkhaledali@gmail.com>
2025-05-29 18:21:00 +0300

    .

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,4 +1,5 @@
 enum Flavor {
+  core,
   apple,
   banana,
 }
@@ -6,27 +7,32 @@ enum Flavor {
 class F {
   static Flavor? appFlavor;
 
-  static String get name => appFlavor?.name ?? 'main';
+  static String get name => appFlavor?.name ?? '';
 
   static String get logo {
     switch (appFlavor) {
+      case Flavor.core:
+        return 'assets/images/core/logo.jpg';
       case Flavor.apple:
         return 'assets/images/apple/logo.png';
       case Flavor.banana:
         return 'assets/images/banana/logo.png';
       default:
-        return 'assets/images/main/logo.jpg';
+        return '';
     }
   }
 
   static String get title {
     switch (appFlavor) {
+      case Flavor.core:
+        return 'Core App';
       case Flavor.apple:
         return 'Apple App';
       case Flavor.banana:
         return 'Banana App';
       default:
-        return 'Main App';
+        return 'title';
     }
   }
+
 }
