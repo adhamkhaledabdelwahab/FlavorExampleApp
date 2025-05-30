ed06eadd5a1b98c48a378f27f9b5ce0df485305d
adhamkhaled <adhamkhaledali@gmail.com>
2025-05-30 18:32:32 +0300

    .

--- switch_pubspec_core.sh
+++ switch_pubspec_core.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FLAVOR="main"
+FLAVOR="core"
 
 if [ "$FLAVOR" == "apple" ]; then
     cp pubspec_apple.yaml pubspec.yaml
 elif [ "$FLAVOR" == "banana" ]; then
     cp pubspec_banana.yaml pubspec.yaml
 else
-    cp pubspec_main.yaml pubspec.yaml
+    cp pubspec_core.yaml pubspec.yaml
 fi
 
 echo "Switched to $FLAVOR flavor"
